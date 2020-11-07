bea732567335a5616b8a79f3c5350a4db49cc4d5
velmurugan ramadasan <vramadas@sg.ibm.com>
2020-11-07 15:41:31 +0800

    modify jupyterconfig to add cpu limit

--- examples/keras_classifier/jupyterhub_config.py
+++ examples/keras_classifier/jupyterhub_config.py
@@ -1,12 +1,1 @@
-import os
-
-import wrapt
-
-from kubernetes.client.configuration import Configuration
-from kubernetes.config.incluster_config import load_incluster_config
-from kubernetes.client.api_client import ApiClient
-from kubernetes.client.rest import ApiException
-from openshift.dynamic import DynamicClient
-
-if os.environ.get('JUPYTERHUB_NOTEBOOK_CPU'):
-    c.Spawner.cpu_limit = os.environ['JUPYTERHUB_NOTEBOOK_CPU']
+c.Spawner.cpu_limit = 0.5
